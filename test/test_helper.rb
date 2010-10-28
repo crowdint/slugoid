acdce8dee7425a5b00e71bfa3b4d113e6b2cbd3b
David Padilla <david@crowdint.com>
2010-10-27 19:39:53 -0500

    Tests for custom parameters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,4 +8,3 @@ require 'acts_as_sluggable'
 
 # $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-
--- test/unit/test_as_sluggable.rb
+++ test/unit/test_as_sluggable.rb
@@ -2,11 +2,19 @@ require 'test_helper'
 
 class Project
   include Mongoid::Document
-  # include Acts::Sluggable
-  
+
   acts_as_sluggable
 
   field :name, :type => String
+end
+
+class Organization
+  include Mongoid::Document
+
+  acts_as_sluggable :alternative_name, :alternative_slug
+
+  field :alternative_name, :type => String
+  field :alternative_slug, :type => String
 end
 
 class TestActsAsSluggable < Test::Unit::TestCase
@@ -19,16 +27,25 @@ class TestActsAsSluggable < Test::Unit::TestCase
     end
   end
 
-  context :acts_as_sluggable do
+  context "default parameters" do
     setup do
       @project = Project.create(:name => 'Some name')
     end
-
     should "create a slug" do
       assert_equal('some-name', @project.slug)
     end
   end
-  
+
+  context "custom parameters" do
+    setup do
+      @organization = Organization.create(:alternative_name => 'Some Other Name')
+    end
+
+    should "create a slug using the custom field and store it on the custom slug field" do
+      assert_equal('some-other-name', @organization.alternative_slug)
+    end
+  end
+
   def teardown
     ::Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
   end
